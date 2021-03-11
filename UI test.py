708ba95c232a78bc3bf31ac99a863edc70b89a78
migouche <migouche.g.r@gmail.com>
2021-03-11 22:28:33 +0100

    can now save image to disk. YESSS

--- UI test.py
+++ UI test.py
@@ -1,5 +1,6 @@
 from pygamemig import *
 import os
+from PIL import Image
 
 window = Window(944, 944)
 bodyIdx = 0
@@ -8,8 +9,8 @@ hairIdx = 0
 bodyPath = "img/bodies/"
 hairPath = "img/hair/"
 
-#pic = Object("img/faces/" + os.listdir("img/faces")[img_idx], Vector2(200, 200))
-#pic.transform.setPos(Vector2(window.width / 2, window.height / 2))
+# pic = Object("img/faces/" + os.listdir("img/faces")[img_idx], Vector2(200, 200))
+# pic.transform.setPos(Vector2(window.width / 2, window.height / 2))
 
 bodyPic = Object(bodyPath + os.listdir(bodyPath)[0], Vector2(800, 800))
 bodyPic.transform.setPos((Vector2(window.width / 2, window.height / 2)))
@@ -19,6 +20,23 @@ hairPic.transform.setPos(Vector2(window.width / 2, window.height / 2))
 
 frame = Object("img/marco.png", Vector2(944, 944))
 frame.transform.setPos(Vector2(window.width / 2, window.height / 2))
+
+
+def Export():  # RGBA
+    body = Image.open(bodyPath + os.listdir(bodyPath)[bodyIdx])
+    hair = Image.open(hairPath + os.listdir(hairPath)[hairIdx])
+
+    result = Image.new("RGBA", (11, 11))
+    pixels = result.load()
+
+    for y in range(11):  # IMG[11y + x]
+        for x in range(11):
+            if hair.getpixel((x, y))[3] == 0:  # place body's pixel
+                pixels[x, y] = body.getpixel((x, y))
+            else:  # place hair's pixel
+                pixels[x, y] = hair.getpixel((x, y))
+    result.save("results/test.png")
+
 
 while window.running:
     if Input.GetKey(K_LEFT) and not left:
@@ -30,6 +48,9 @@ while window.running:
         hairIdx -= 1
     if Input.GetKey(K_d) and not d:
         hairIdx += 1
+
+    if Input.GetKey(K_SPACE) and not space:
+        Export()
 
     if bodyIdx >= len(os.listdir(bodyPath)):
         bodyIdx = 0
@@ -45,9 +66,9 @@ while window.running:
     right = Input.GetKey(K_RIGHT)
     a = Input.GetKey(K_a)
     d = Input.GetKey(K_d)
+    space = Input.GetKey(K_SPACE)
 
     bodyPic.SetImg(bodyPath + os.listdir(bodyPath)[bodyIdx])
     hairPic.SetImg(hairPath + os.listdir(hairPath)[hairIdx])
+
     window.Update()
-
-
