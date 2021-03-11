2e1311752b38dd54d16c101b51c5127ddec7754c
migouche <migouche.g.r@gmail.com>
2021-03-11 20:26:17 +0100

    basic hair and body iteration

--- UI test.py
+++ UI test.py
@@ -2,26 +2,52 @@ from pygamemig import *
 import os
 
 window = Window(800, 600)
-img_idx = 0
-img_path = str(os.listdir("img/faces")[0])
-print(img_path)
+bodyIdx = 0
+hairIdx = 0
 
-pic = Object("img/faces/" + os.listdir("img/faces")[img_idx], Vector2(200, 200))
-pic.transform.setPos(Vector2(window.width / 2, window.height / 2))
+bodyPath = "img/bodies/"
+hairPath = "img/hair/"
+
+#pic = Object("img/faces/" + os.listdir("img/faces")[img_idx], Vector2(200, 200))
+#pic.transform.setPos(Vector2(window.width / 2, window.height / 2))
+
+bodyPic = Object(bodyPath + os.listdir(bodyPath)[0], Vector2(200, 200))
+bodyPic.transform.setPos((Vector2(window.width / 2, window.height / 2)))
+
+hairPic = Object(hairPath + os.listdir(hairPath)[0], Vector2(200, 200))
+hairPic.transform.setPos(Vector2(window.width / 2, window.height / 2))
+
+frame = Object("img/marco.png", Vector2(236, 236))
+frame.transform.setPos(Vector2(window.width / 2, window.height / 2))
 
 while window.running:
     if Input.GetKey(K_LEFT) and not left:
-        img_idx -= 1
+        bodyIdx -= 1
     if Input.GetKey(K_RIGHT) and not right:
-        img_idx += 1
-    if img_idx >= len(os.listdir("img/faces")):
-        img_idx = 0
-    elif img_idx < 0:
-        img_idx = len(os.listdir("img/faces")) - 1
+        bodyIdx += 1
+
+    if Input.GetKey(K_a) and not a:
+        hairIdx -= 1
+    if Input.GetKey(K_d) and not d:
+        hairIdx += 1
+
+    if bodyIdx >= len(os.listdir(bodyPath)):
+        bodyIdx = 0
+    elif bodyIdx < 0:
+        bodyIdx = len(os.listdir(bodyPath)) - 1
+
+    if hairIdx >= len(os.listdir(hairPath)):
+        hairIdx = 0
+    elif hairIdx < 0:
+        hairIdx = len(os.listdir(hairPath)) - 1
 
     left = Input.GetKey(K_LEFT)
     right = Input.GetKey(K_RIGHT)
-    pic.SetImg("img/faces/" + os.listdir("img/faces")[img_idx])
+    a = Input.GetKey(K_a)
+    d = Input.GetKey(K_d)
+
+    bodyPic.SetImg(bodyPath + os.listdir(bodyPath)[bodyIdx])
+    hairPic.SetImg(hairPath + os.listdir(hairPath)[hairIdx])
     window.Update()
 
 
